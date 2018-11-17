2d870aab1921a735ea4493bddff40d98ac524966
Unknown <tramkotipo_18@hotmail.com>
2018-11-17 16:40:37 -0500

    Added pug files, product list still not showing and nav bar not working properly yet

--- TP4/routes/index.js
+++ TP4/routes/index.js
@@ -2,7 +2,35 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "Accueil", message: "Ça semble fonctionner!" });
+    res.render("index", { title: "OnlineShop - Accueil" });
+});
+
+router.get("/accueil", (req, res) => {
+    res.render("index", { title: "OnlineShop - Accueil" });
+});
+
+router.get("/produits", (req, res) => {
+    res.render("products", { title: "OnlineShop - Produits" });
+});
+
+router.get("/produits/:id", (req, res) => {
+    res.render("product", { title: "OnlineShop - Produit", id: req.params[0]}) // À voir!
+});
+
+router.get("/contact", (req, res) => {
+    res.render("contact", { title: "OnlineShop - Contact" });
+});
+
+router.get("/panier", (req, res) => {
+    res.render("shopping-cart", { title: "OnlineShop - Panier" });
+});
+
+router.get("/commande", (req, res) => {
+    res.render("order", { title: "OnlineShop - Commande" });
+});
+
+router.get("/confirmation", (req, res) => {
+    res.render("confirmation", { title: "OnlineShop - Confirmation" });
 });
 
 module.exports = router;
