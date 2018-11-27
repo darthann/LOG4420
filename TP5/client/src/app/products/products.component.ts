a53c66ce3b19ab07ce9c2b90131fe6081d7e8bda
Unknown <tramkotipo_18@hotmail.com>
2018-11-27 17:06:19 -0500

    Products page done

--- TP5/client/src/app/products/products.component.ts
+++ TP5/client/src/app/products/products.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product, ProductsService } from 'app/products.service';
 
 /**
  * Defines the component responsible to manage the display of the products page.
@@ -7,6 +8,49 @@ import { Component } from '@angular/core';
     selector: 'products',
     templateUrl: './products.component.html'
 })
-export class ProductsComponent {
-    // TODO: À compléter
+export class ProductsComponent implements OnInit {
+    private products: Product[];
+    private category: string;
+    private criteria: string;
+
+    constructor(private productsService: ProductsService) {
+        this.products = [];
+        this.category = "all";
+        this.criteria = "price-asc";
+    }
+
+    ngOnInit() {
+        this.getProducts();
+    }
+
+    private changeCategory(category: string) : void {
+        this.category = category;
+        this.getProducts();
+    }
+
+    private changeCriteria(criteria: string) : void {
+        this.criteria = criteria;
+        this.getProducts();
+    }
+
+    private getProducts() : void {
+        this.productsService.getProducts(this.criteria, this.category).then((response) => {
+            this.products = [];
+            response.forEach(product => {
+                let tempProduct = new Product;
+
+                tempProduct.id = product.id;
+                tempProduct.name = product.name;
+                tempProduct.price = product.price;
+                tempProduct.image = product.image;
+                tempProduct.category = product.category;
+                tempProduct.description = product.description;
+                tempProduct.features = product.features;
+
+                this.products.push(tempProduct);
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
 }
